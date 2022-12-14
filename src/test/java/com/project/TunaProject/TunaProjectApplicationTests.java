c41cb719aea5d03baba2ae7be2419e0880ac1f77
hhj <gurwn805@naver.com>
2022-12-14 09:58:52 +0900

    first commit

---
+++ src/test/java/com/project/TunaProject/TunaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TunaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
