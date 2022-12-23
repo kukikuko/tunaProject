17d11761942b44f71f89ea92800a744f64faa186
gyeoli <eungyeoly93@gmail.com>
2022-12-23 16:47:08 +0900

    조회수,카테고리작업

--- src/main/java/com/project/TunaProject/repository/mybatis/MybatisPostRepository.java
+++ src/main/java/com/project/TunaProject/repository/mybatis/MybatisPostRepository.java
@@ -21,8 +21,10 @@ public class MybatisPostRepository implements PostRepository{
 	private final PostItemMapper postItemMapper;
 
 	@Override
-	public Post insert(Post post) {
+	public Post insert(Post post, int membercode) {
 		// TODO Auto-generated method stub
+//		Post postItem = postItemMapper.insert(post, membercode);
+		post.setPMemCode(membercode);
 		Integer result = postItemMapper.insert(post);
 		return post;
 	}
@@ -31,6 +33,7 @@ public class MybatisPostRepository implements PostRepository{
 	public Post selectByPostCode(String postCode) {
 		// TODO Auto-generated method stub
 		Post post = postItemMapper.selectByPostCode(postCode);
+
 		return post;
 	}
 
@@ -43,10 +46,10 @@ public class MybatisPostRepository implements PostRepository{
 
 	@Override
 	@Transactional
-	public boolean update(String postCdoe, Post post) {
+	public boolean update(String postCode, Post post) {
 		// TODO Auto-generated method stub
 		boolean result = false;
-		postItemMapper.update(postCdoe, post);
+		postItemMapper.update(postCode, post);
 		result = true;
 		
 		return result;
@@ -56,6 +59,12 @@ public class MybatisPostRepository implements PostRepository{
 	public void deleteByPostCode(String postCode) {
 		// TODO Auto-generated method stub
 		postItemMapper.deleteByPostCode(postCode);
+	}
+
+	@Override
+	public void viewCont(String postCode) {
+		// TODO Auto-generated method stub
+		postItemMapper.viewCont(postCode);
 	}
 		
 }
