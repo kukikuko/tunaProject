dae358fda16b472d99b8afb52578b6163b7d5f82
yijh0315 <yijh0315@naver.com>
2022-12-27 14:29:08 +0900

    비밀번호 변경 기능 완료

--- src/main/java/com/project/TunaProject/controller/MemberController.java
+++ src/main/java/com/project/TunaProject/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.project.TunaProject.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.autoconfigure.cassandra.CassandraProperties.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -31,32 +36,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/myPage")
 public class MemberController {
 	
-	private final SessionManager sessionManager;
 	private final MemberRepository memberRepository;
-	private final Tuna_LoginService loginService;
-
-	@GetMapping("/join")
-	public String join() {
-		return "join/join";
-	}
-	
-	@PostMapping("/join")
-	public String memberInsert(@ModelAttribute MemberVO memberVO) {
-		
-		
-		memberVO.setMemberMail(memberVO.getMemberMail1()+"@"+memberVO.getMemberMail2());
-		memberVO.setMemberPN(String.valueOf(memberVO.getMemberPN1())+"-"+String.valueOf(memberVO.getMemberPN2())+"-"+String.valueOf(memberVO.getMemberPN3()));
-		memberRepository.memberInsert(memberVO);
-		
-		log.info("member {}", memberVO);
-		return "redirect:/";
-	}
-	
 	
 	//개인정보 변경
-	@GetMapping("/myPage/memberUpdate")
+	@GetMapping("/memberUpdate")
 	public String memberUpdate(HttpServletRequest req, Model model) {
 		HttpSession session = req.getSession(false);
 		
@@ -69,7 +55,7 @@ public class MemberController {
 		return "myPage/memberUpdate";
 	}
 	
-	@PostMapping("/myPage/memberUpdate")
+	@PostMapping("/memberUpdate")
 	public String updateMemberByEmail(MemberVO memberVO, HttpServletRequest req) {
 		
 		HttpSession session = req.getSession(false);
@@ -85,7 +71,7 @@ public class MemberController {
 	}
 	
 	//비밀번호 변경	
-	@GetMapping("/myPage/passwordUpdate")
+	@GetMapping("/passwordUpdate")
 	public String passwordUpdate(HttpServletRequest req, Model model) {
 		HttpSession session = req.getSession(false);
 		
@@ -98,24 +84,46 @@ public class MemberController {
 	}
 
 	
-	//1225
-	@PostMapping("/myPage/passwordUpdate")
-	public String updatePassword(@ModelAttribute LoginForm loginForm,
-			BindingResult bindingResult, HttpServletResponse resp
+	//비밀번호 변경
+	@PostMapping("/passwordUpdate")
+	public String updatePassword(@ModelAttribute LoginForm loginForm
 			, HttpServletRequest req, MemberVO memberVO
 			, @RequestParam(name="redirectURL", defaultValue = "/") String redirectURL ) {
 		
 		HttpSession session = req.getSession(false);
-
+		
 		MemberVO tempVO = (MemberVO)session.getAttribute(SessionVar.LOGIN_MEMBER);
 		memberVO.setMemberMail(tempVO.getMemberMail());
 		
-		log.info("update memberVO {}", memberVO);
+		log.info("update PW memberVO {}", memberVO);
 		
 		memberRepository.updatePassword(memberVO);
 		
 	 		return "redirect:/";
 	}
+	
+	@PostMapping("/pwcheck")
+	@ResponseBody
+	public Object pwCheck(HttpServletRequest req
+			,@RequestParam String password) {
+		
+		log.info("{}", password);
+		
+		Map<String, Integer> result = new HashMap<>(); 
+		
+		HttpSession session = req.getSession(false);
+		
+		MemberVO tempVO = (MemberVO)session.getAttribute(SessionVar.LOGIN_MEMBER);
+		if(tempVO.getPassword().equals(password)) {
+			result.put("result", 1);
+			return result;
+		} else {
+			result.put("result", 2);
+			return result;
+		}
+		
+	}
+	
 	public void validateLoginForm(LoginForm loginForm, Errors errors) {
 		if(!StringUtils.hasText(loginForm.getEmail())) {
 			errors.rejectValue("loginId", null, "아이디 필수 입력입니다.");
@@ -125,8 +133,9 @@ public class MemberController {
 			errors.rejectValue("password", null, "비밀번호 필수 입력입니다.");
 		}
 	}
+	
 	//회원 탈퇴
-	@GetMapping("/myPage/memberOut")
+	@GetMapping("/memberOut")
 	public String memberOut() {
 		return "myPage/memberOut";
 	}
