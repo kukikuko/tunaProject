ae72afc84ba816c75f435ca73cbd7e1edab41f95
gyeoli <eungyeoly93@gmail.com>
2022-12-29 16:48:31 +0900

    상단바세션유지

--- src/main/java/com/project/TunaProject/repository/mybatis/MybatisPostRepository.java
+++ src/main/java/com/project/TunaProject/repository/mybatis/MybatisPostRepository.java
@@ -70,5 +70,12 @@ public class MybatisPostRepository implements PostRepository{
 		// TODO Auto-generated method stub
 		postItemMapper.viewCont(postCode);
 	}
+
+	@Override
+	public List<Post> selectSearch(String keyword) {
+		// TODO Auto-generated method stub
+		List<Post> postList = postItemMapper.selectSearch(keyword);
+		return postList;
+	}
 		
 }
