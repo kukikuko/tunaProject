bded71e329a449bb90419e9760106f47a1747aae
gyeoli <eungyeoly93@gmail.com>
2023-01-02 12:38:24 +0900

    update  카테고리 적용완료

--- src/main/java/com/project/TunaProject/repository/PostRepository.java
+++ src/main/java/com/project/TunaProject/repository/PostRepository.java
@@ -13,7 +13,7 @@ public interface PostRepository {
 	
 	public List<Post> selectAll();
 	
-	public boolean update(String postCode, Post post);
+	public boolean update(String postCode, Post post,  String ctCode);
 	
 	public boolean updateDelete(String postCode);
 	
